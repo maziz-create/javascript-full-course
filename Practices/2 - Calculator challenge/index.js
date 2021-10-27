e6d93fb8ca3f35bebc21dd37a5dbadb0ce18f9ec
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-10-27 18:59:54 +0300

    Practices - 2 | Selected nodes with ids num1-el, num2-el and sum-el. Then number 1 and number 2 are setted. Then added 4 functions for related processes.

--- Practices/2 - Calculator challenge/index.js
+++ Practices/2 - Calculator challenge/index.js
@@ -12,3 +12,29 @@
  * Kullanıcı aynı isimli butonlara tıkladığında ilgili fonksiyonu çağır ve 
  * sonucu 'sum-el' id'sine sahip span etiketinde göster.
  */
+
+let num1El = document.getElementById('num1-el');
+let num2El = document.getElementById('num2-el');
+let sumEl = document.getElementById('sum-el');
+
+num1El.textContent = 60;
+num2El.textContent = 12;
+
+// console.log(parseInt(num1El.textContent));
+
+const add = () => {
+    sumEl.textContent = parseInt(num1El.textContent) + parseInt(num2El.textContent);
+}
+
+const subtract = () => {
+    sumEl.textContent = parseInt(num1El.textContent) - parseInt(num2El.textContent);
+}
+
+const divide = () => {
+    sumEl.textContent = parseFloat(num1El.textContent) / parseFloat(num2El.textContent);
+}
+
+const multipy = () => {
+    sumEl.textContent = parseInt(num1El.textContent) * parseInt(num2El.textContent);
+}
+
