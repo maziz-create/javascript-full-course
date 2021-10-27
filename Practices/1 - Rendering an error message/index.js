3e85ff6921848ef5fd4a660fe22698d3b1074536
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-10-27 18:25:42 +0300

    Practices - 1 | Added html and css codes copied from freeCodeCamp github repository.

---
+++ Practices/1 - Rendering an error message/index.js
@@ -0,0 +1,10 @@
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error".
+
+/**
+ * Türkçe:
+ * Kullanıcı 'Purchase' butonuna tıklarsa eğer
+ * 'error' id'sine sahip olan p etiketine
+ * 'Something went wrong, please try again' ifadelerini yazdır.
+ */
