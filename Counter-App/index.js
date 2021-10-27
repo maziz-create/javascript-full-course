5f3c2d3095ef7407cb180a0d61986e502b60c680
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-10-27 17:40:12 +0300

    Previously saved entries are displayed in the 'Previous entries' section with localStorage

--- Counter-App/index.js
+++ Counter-App/index.js
@@ -1,10 +1,16 @@
 let countEl = document.getElementById('count-el');
+let saveEl = document.getElementById('save-el');
 
 let count = !JSON.parse(localStorage.getItem('count'))
     ? 0
     : JSON.parse(localStorage.getItem('count'));
 
+let previousEntries = !localStorage.getItem('previousEntries')
+    ? 'Previous entries: '
+    : localStorage.getItem('previousEntries');
+
 countEl.innerText = count;
+saveEl.innerText = previousEntries;
 
 const increment = () => {
     count++;
@@ -13,4 +19,6 @@ const increment = () => {
 
 const save = () => {
     localStorage.setItem('count', count);
+    saveEl.innerHTML += count + " - ";
+    localStorage.setItem('previousEntries', saveEl.innerHTML);
 }
